804cb73b9aaad3e67e6a95735ecfa326e9361028
Dural Keys <duralinitaly@gmail.com>
2020-02-17 12:53:22 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -6,7 +6,8 @@ puts "Wingardium Leviosa."
 counter += 1
 
 end
-____________________________________
+end
+___________________________________
 #counter = 0
 #while counter < 20
  # puts "The current number is less than 20."
