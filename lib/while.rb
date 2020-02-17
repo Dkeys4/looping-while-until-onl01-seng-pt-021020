9fcbbe1dd528cfc5ac5dedc15bd6473763f0353a
Dural Keys <duralinitaly@gmail.com>
2020-02-17 12:44:07 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,15 +1,17 @@
 def using_while
   levitation_force = 6
 	
+	counter = 0 
+while counter < 10 
+puts "Wingardium Leviosa"
+
+
+counter += 1
 
 end
-
-=+1
-
-end
-
-counter = 0
-while counter < 20
-  puts "The current number is less than 20."
-  counter += 1
-end
+____________________________________
+#counter = 0
+#while counter < 20
+ # puts "The current number is less than 20."
+  #counter += 1
+#end
